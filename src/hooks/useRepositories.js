e57b37d79e62a6f62ff274565d2bcdf0d0ee9750
Jaakko Lehtonen <>
2021-11-19 20:04:55 +0200

    Complete exercise 10.11

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,15 @@
+import { useQuery } from "@apollo/client";
+import { GET_REPOSITORIES } from "../graphql/queries";
+
+const useRepositories = () => {
+  const { data, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+  });
+
+  return {
+    repositories: data ? data.repositories : null,
+    loading,
+  };
+};
+
+export default useRepositories;
